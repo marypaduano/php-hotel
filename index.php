0335c39f32e1061f9513816b2f60cf0727cc7e88
marypaduano <maria.paduano@gmail.com>
2023-03-20 20:00:54 +0100

    fix table

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $hotels = [
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h1></h1>
+                        <h1>Tabella Hotel</h1>
                     </div>
                     <div class="card-body">
                         <table class="table table-bordered">
